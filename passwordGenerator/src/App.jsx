d62a30c6cc0fa7893c6c5809ec12a0bb8c673822
Shivish468 <shivamaryan468@gmail.com>
2024-01-10 22:24:43 +0530

    writing the code for copy password to clipboard

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState, useCallback, useEffect } from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
 
 function App() {
   const [length, setLength] = useState(8)
   const [numberAllowed, setNumberAllowed] = useState(false)
   const [charAllowed, setCharAllowed] = useState(false)
   const [password, setPassword] = useState("")
+
+  const passwordRef = useRef(null)
 
   const passwordGenerator = useCallback(() => {
     let pass = ""
@@ -16,7 +18,7 @@ function App() {
       str += "!@#$%^&*()"
     }
 
-    for (let i = 0; i <= length; i++) {
+    for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1)
       pass += str.charAt(char)
     }
@@ -25,22 +27,30 @@ function App() {
 
   }, [length, numberAllowed, charAllowed, setPassword])
 
+  const copyPassword = useCallback(() => {
+    passwordRef.current?.select()
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
   useEffect(() => {passwordGenerator()}, [length, numberAllowed, charAllowed, passwordGenerator])
 
   return (
     <>
       
       <div className=' w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-600'>
-      <h1 className='text-xl text-center text-black my-3'>Password Generator</h1>
+      <h1 className='text-2xl text-center text-black my-3'>Password Generator</h1>
         <div className='"flex shadow rounded-lg overflow-hidden mb-4'>
           <input
-          type='text'
-          value={password}
-          className=' outline-none w-full py-1 px-3'
-          placeholder='password' 
-          readOnly
+            type='text'
+            value={password}
+            className=' outline-none w-full py-1 px-3'
+            placeholder='password' 
+            readOnly
+            ref={passwordRef}
           />
-          <button className='outline-none bg-blue-600 text-white px-3 py-0.5 shrink-0'>copy</button>
+          <button
+          onClick={copyPassword} 
+          className='outline-none bg-blue-600 text-white px-3 py-0.5 shrink-0'>copy</button>
         </div>
         <div className=' flex text-sm gap-x-2'>
           <div className='flex items-center gap-x-1'>
