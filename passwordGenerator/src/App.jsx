d954e827ac376a27d52d38d0d5b40192d3992f80
Shivish468 <shivamaryan468@gmail.com>
2024-01-10 22:36:03 +0530

    project completed with complete UI/UX

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -36,9 +36,8 @@ function App() {
 
   return (
     <>
-      
       <div className=' w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-600'>
-      <h1 className='text-2xl text-center text-black my-3'>Password Generator</h1>
+        <h1 className='text-2xl text-center text-black my-3'>Password Generator</h1>
         <div className='"flex shadow rounded-lg overflow-hidden mb-4'>
           <input
             type='text'
@@ -50,7 +49,8 @@ function App() {
           />
           <button
           onClick={copyPassword} 
-          className='outline-none bg-blue-600 text-white px-3 py-0.5 shrink-0'>copy</button>
+          className='outline-none bg-blue-600 text-white px-3 py-0.5 shrink-0'
+          >copy</button>
         </div>
         <div className=' flex text-sm gap-x-2'>
           <div className='flex items-center gap-x-1'>
@@ -88,7 +88,6 @@ function App() {
           </div>
         </div>
       </div>
-
     </>
   )
 }
