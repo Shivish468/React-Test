9009f9714eca4f3de5dd6f4ac78a75390de311ca
Shivish468 <shivamaryan468@gmail.com>
2024-01-25 21:19:29 +0530

    use of usestate hook

--- test-app/src/Counter.js
+++ test-app/src/Counter.js
@@ -4,13 +4,13 @@ function Counter() {
     const [y, setY] = useState(0);
     return (
         <>
-            Count X: {x} is {(x%2 == 0) ? 'Even' : 'Odd'}
+            Count X: {x} is {(x%2 === 0) ? 'Even' : 'Odd'}
             <button onClick={() => setX(x+1)}>Inc</button>
             <button onClick={() => setX(x-1)}>Dec</button>
             <br />
-            Count Y: {y} is {(y%2 == 0) ? 'Even' : 'Odd'}
-            <button onClick={() => setX(y+1)}>Inc</button>
-            <button onClick={() => setX(y-1)}>Dec</button>
+            Count Y: {y} is {(y%2 === 0) ? 'Even' : 'Odd'}
+            <button onClick={() => setY(y+1)}>Inc</button>
+            <button onClick={() => setY(y-1)}>Dec</button>
         </>
     );
 }
