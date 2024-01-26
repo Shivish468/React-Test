965dd7d0b355eb306de64090876c7d777dc9f2a9
Shivish468 <shivamaryan468@gmail.com>
2024-01-26 17:42:27 +0530

    github function and styling

--- reactRouter/src/components/Github/Github.jsx
+++ reactRouter/src/components/Github/Github.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Github() {
+  const [data, setData] = useState([])
+    useEffect(() => {
+     fetch('https://api.github.com/users/Shivish468')
+     .then(response => response.json())
+     .then(data => {
+        console.log(data);
+        setData(data)
+     })
+    }, [])
+    
   return (
-    <div>Github</div>
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
+    <img src={data.avatar_url} alt="Git picture" width={300} />
+    </div>
   )
 }
 
